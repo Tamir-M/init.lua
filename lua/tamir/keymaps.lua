3668ccced646c33b633c3b32bcf853a0b172d223
Tamir-M <tamirmousari@gmail.com>
2024-06-08 14:52:57 +0300

    Added some basic remappings

---
+++ lua/tamir/keymaps.lua
@@ -0,0 +1,15 @@
+vim.g.mapleader = " "
+
+-- Quick Ex
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex);
+
+-- Remove arrow keys completely.
+vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!"<CR>')
+vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!"<CR>')
+vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!"<CR>')
+vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!"<CR>')
+
+vim.keymap.set("i", "<left>", '<cmd>echo "Use h to move!"<CR>')
+vim.keymap.set("i", "<right>", '<cmd>echo "Use l to move!"<CR>')
+vim.keymap.set("i", "<up>", '<cmd>echo "Use k to move!"<CR>')
+vim.keymap.set("i", "<down>", '<cmd>echo "Use j to move!"<CR>')
