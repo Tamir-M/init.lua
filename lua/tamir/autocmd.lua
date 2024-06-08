5a45b8bc31ad133aaf9a069e4eb94b4f8067d50f
Tamir-M <tamirmousari@gmail.com>
2024-06-08 19:43:22 +0300

    Added docs to personal config files

--- lua/tamir/autocmd.lua
+++ lua/tamir/autocmd.lua
@@ -1,3 +1,4 @@
+-- Highlight selection on yank
 vim.api.nvim_create_autocmd('TextYankPost', {
   group = vim.api.nvim_create_augroup('highlight_yank', {}),
   desc = 'Hightlight selection on yank',
--- lua/tamir/general.lua
+++ lua/tamir/general.lua
@@ -1,6 +1,8 @@
+-- Line numbers + relative
 vim.wo.number = true
 vim.wo.relativenumber = true
 
+-- Tab width 2
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
