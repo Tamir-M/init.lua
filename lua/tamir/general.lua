1c55efa500f3557dcb21cecc37558f2907ed55a0
Tamir-M <tamirmousari@gmail.com>
2024-06-11 11:40:04 +0300

    Made line numbers white instead of default gray.

--- lua/tamir/general.lua
+++ lua/tamir/general.lua
@@ -2,6 +2,10 @@
 vim.wo.number = true
 vim.wo.relativenumber = true
 
+-- Color of line number 
+vim.api.nvim_set_hl(0, 'LineNr', { fg = "white" })
+
+
 -- Tab width 2
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
