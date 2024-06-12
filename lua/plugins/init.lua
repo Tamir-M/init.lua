630aebffa01074fd526a3c7942c1cb4f99b7e3ce
Tamir-M <tamirmousari@gmail.com>
2024-06-12 11:41:20 +0300

    Added treesitter highlighting for all the languages I use

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,3 +1,4 @@
 require('plugins.transparent')
 require('plugins.telescope')
 require('plugins.lsp')
+require('plugins.treesitter')
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,6 @@
+require('nvim-treesitter.configs').setup({
+  ensure_installed = {"rust", "lua", "javascript"},
+  highlight = {
+    enable = true,
+  }
+})
